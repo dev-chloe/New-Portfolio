e8a65f555b8f38f87f21ca46d160f820ea085156
dev-chloe <chloekim.4969@gmail.com>
2020-04-22 16:46:52 +0900

    contact style change

--- Desktop/PORTFOLIO/src/app/contact/contact.component.ts
+++ Desktop/PORTFOLIO/src/app/contact/contact.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  emailflag: boolean = false;
+  messageflag: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  emailcheck(event) {
+    if(event.target.value != '') {
+      this.emailflag = true;
+    }
+    else{
+      this.emailflag = false;
+    }
+  }
+
+  messagecheck(event) {
+    if(event.target.value != '') {
+      this.messageflag = true;
+    }
+    else{
+      this.messageflag = false;
+    }
   }
 
 }
