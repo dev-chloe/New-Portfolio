c900d9f9610c7c56f330fa2ef47f27e062ce4bfe
dev-chloe <chloekim.4969@gmail.com>
2020-04-22 23:40:24 +0900

    what i do form  by  ngx-chart

--- Desktop/PORTFOLIO/src/app/app-routing.module.ts
+++ Desktop/PORTFOLIO/src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { WhatIDoComponent } from './what-i-do/what-i-do.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'main', pathMatch: 'full'},
   {path: 'main', component: MainComponent},
   {path: 'about', component: AboutComponent},
   {path: 'contact', component: ContactComponent},
+  {path: 'what-i-do', component: WhatIDoComponent},
+  {path: '**', redirectTo: 'main', pathMatch: 'full'}
 ];
 
 @NgModule({
--- Desktop/PORTFOLIO/src/app/app.module.ts
+++ Desktop/PORTFOLIO/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,7 @@ import { CursorComponent } from './cursor/cursor.component';
 import { MainComponent } from './main/main.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { WhatIDoComponent } from './what-i-do/what-i-do.component';
 
 @NgModule({
   declarations: [
@@ -17,11 +19,13 @@ import { ContactComponent } from './contact/contact.component';
     CursorComponent,
     MainComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    WhatIDoComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    BrowserAnimationsModule,
+    NgxChartsModule,
     AppRoutingModule
   ],
   providers: [],
